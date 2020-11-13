509bd5a790184ce2b3e32934f0a2de0bbab8ec72
Matt Haywood <matthaywood2012@gmail.com>
2020-11-13 19:51:18 +0000

    Embedded Matplotlib charts into the tkinter window, Also added input for port and ip connection

--- ComputerGUI/tkinter_test.py
+++ ComputerGUI/tkinter_test.py
@@ -1,22 +1,41 @@
-from tkinter import Tk, Label, Button, Entry, Frame, BOTH, LEFT, RIGHT, Text, END
+from tkinter import Tk, Label, Button, Entry, Frame, BOTH, LEFT, RIGHT, Text, END, TOP
 import socketio
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
 
 
 sio = socketio.Client()
 
 @sio.event
 def connect():
-	print('Connection Established')
+    print('Connection Established')
+
+@sio.event
+def disconnect():
+    print('Disconnected from Server')
 
 @sio.on("new-data")
 def on_new_data(data):
-	print("new data received")
-	print(data)
-	my_gui.data_view.insert(END, f"{data} \n")
+    print("new data received")
+    print(data)
+    my_gui.data_view.insert(END, f"{data} \n")
 
 
 class MyFirstGUI:
     def __init__(self, master):
+
+        fig=Figure(figsize=(5,4), dpi=100)
+        t = np.arange(0, 3, .01)
+        fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+
+        canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+
+
         self.master = master
         master.title("Telemetry Data Viewer")
 
@@ -28,8 +47,6 @@ class MyFirstGUI:
         self.frame1 = Frame(master)
         self.frame1.pack(fill=BOTH, expand=True)
 
-        
-        
         self.connect_button = Button(self.frame1, text="Connect", command=self.connect)
         self.connect_button.pack()
 
@@ -37,10 +54,14 @@ class MyFirstGUI:
         self.label3.pack(side=LEFT)
 
         self.port_entry = Entry(self.frame1)
-       	self.port_entry.pack(side=LEFT)
-      	
+        self.port_entry.pack(side=LEFT)
+        
         self.label2 = Label(self.frame1, text="Enter Server Ip")
         self.label2.pack(side=LEFT)
+
+        self.connection_status = Label(self.frame1, text="Not Connected", fg="red")
+        self.connection_status.pack(side=LEFT)
+        
 
 
         self.ip_entry = Entry(self.frame1)
@@ -56,12 +77,29 @@ class MyFirstGUI:
     def greet(self):
         print("Greetings!")
 
-    def connect(self):		#Connect to the socket server 
-    	ip = self.ip_entry.get()
-    	port = self.port_entry.get()
-    	print(f"Connecting to server @{ip}:{port}")
-    	sio.connect(f'http://localhost:8080')
-    	
+    def connect(self):      #Connect to the socket server 
+        ip = self.ip_entry.get()
+        port = self.port_entry.get()
+        print(f"Connecting to server @{ip}:{port}")
+        address = 'http://'+ip+":"+port
+        print(address)
+        sio.connect(address)
+        self.connection_status.config(fg="green")
+        self.connection_status.config(text=f"Connected to server@{ip}:{port}")
+        self.connect_button.config(text="Disconnect", command=self.disconnect)
+
+    def disconnect(self):
+        sio.disconnect()
+        self.connect_button.config(text="Connect", command=self.connect)
+        self.connection_status.config(fg="red", text="Not Connected")
+
+
+
+    def plot_update(self):
+        #Functions for updating the matplotlib graphs
+        pass
+
+        
 
 root = Tk()
 my_gui = MyFirstGUI(root)
